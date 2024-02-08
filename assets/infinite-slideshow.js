cdd8d3e4655aee5c61e82974bd14fdb180cbdb22
rlotto <riky.lotto@gmail.com>
2024-02-08 15:55:52 +0100

    Fix autoplay delay in infinite slideshow

--- assets/infinite-slideshow.js
+++ assets/infinite-slideshow.js
@@ -3,27 +3,26 @@ let SwiperTop = new Swiper('.swiper--top', {
     centeredSlides: true,
     speed: 6000,
     autoplay: {
-      delay: 1,
+        delay: 1,
     },
     loop: true,
-    slidesPerView:'auto',
+    slidesPerView: 'auto',
     allowTouchMove: false,
     disableOnInteraction: true
-  });
-  
-  let SwiperBottom = new Swiper('.swiper--bottom', {
+});
+
+let SwiperBottom = new Swiper('.swiper--bottom', {
     spaceBetween: 0,
     centeredSlides: true,
     speed: 6000,
     autoplay: {
-      delay: 1,
-      reverseDirection: true
+        delay: 1,
+        reverseDirection: true
     },
     loop: true,
     loopedSlides: 4,
-    slidesPerView:'auto',
+    slidesPerView: 'auto',
     allowTouchMove: false,
     disableOnInteraction: true
-  });
-  
-  
+});
+
