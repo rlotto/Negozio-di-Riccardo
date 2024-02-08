3be6944e6caf40b8b3a3c858545d3d63408e93fb
rlotto <riky.lotto@gmail.com>
2024-02-08 15:46:45 +0100

    Update infinite slideshow styles and structure

---
+++ assets/infinite-slideshow.js
@@ -0,0 +1,13 @@
+let SwiperTop = new Swiper('.swiper--top', {
+    spaceBetween: 0,
+    centeredSlides: true,
+    speed: 6000,
+    autoplay: {
+      delay: 1,
+    },
+    loop: true,
+    slidesPerView:'auto',
+    allowTouchMove: false,
+    disableOnInteraction: true
+});
+  
