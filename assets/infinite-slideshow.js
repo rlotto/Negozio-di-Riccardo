35247f1bba49a85c5ba6d86306fbfecf4ddb6f05
rlotto <riky.lotto@gmail.com>
2024-02-13 12:46:50 +0100

    Refactor infinite slideshow animation

--- assets/infinite-slideshow.js
+++ assets/infinite-slideshow.js
@@ -1,34 +1,31 @@
-
-
 document.addEventListener('DOMContentLoaded', (event) => {
     const carouselContainer = document.getElementById('carouselContainer');
 
-    // Clone the carousel content to create a continuous loop
-    const carouselItems = carouselContainer.innerHTML;
-    carouselContainer.innerHTML += carouselItems;
+// Clone the carousel content to create a continuous loop
+const carouselItems = carouselContainer.innerHTML;
+carouselContainer.innerHTML += carouselItems;
 
-    // Set up animation
-    let scrollLeft = 0;
-    const scrollSpeed = 5; // Adjust the scroll speed as needed
+// Set up animation
+let scrollLeft = 0;
+const scrollSpeed = 4; // Adjust the scroll speed as needed
 
-    let lastTimestamp = null;
-
-    function animateCarousel(timestamp) {
-        if (!lastTimestamp) {
-            lastTimestamp = timestamp;
-        }
-
-        const deltaTime = timestamp - lastTimestamp;
+function animateCarousel(timestamp) {
+    if (!lastTimestamp) {
         lastTimestamp = timestamp;
-
-        scrollLeft += scrollSpeed * deltaTime / 60; // Normalize speed
-        if (scrollLeft >= carouselContainer.scrollWidth / 2) {
-            scrollLeft = 0;
-        }
-        carouselContainer.style.transform = `translateX(-${scrollLeft}px)`;
-
-        requestAnimationFrame(animateCarousel);
     }
 
+    const deltaTime = timestamp - lastTimestamp;
+    lastTimestamp = timestamp;
+
+    scrollLeft += scrollSpeed * deltaTime / 60; // Normalize speed
+    if (scrollLeft >= carouselContainer.scrollWidth / 2) {
+        scrollLeft = 0;
+    }
+    carouselContainer.style.transform = `translateX(-${scrollLeft}px)`;
+
     requestAnimationFrame(animateCarousel);
+}
+
+let lastTimestamp = null;
+requestAnimationFrame(animateCarousel);
 });
